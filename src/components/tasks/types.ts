74477681abd8d5ac3b4ef1fcc09aaaab731a2d98
G Vishok <vishokprasad27@gmail.com>
2025-06-19 12:29:16 +0530

    added types

---
+++ src/components/tasks/types.ts
@@ -0,0 +1,36 @@
+export interface Assignee {
+  id: string;
+  name: string;
+}
+
+export enum TaskStatus {
+  Todo = 'todo',
+  InProgress = 'inProgress',
+  Completed = 'completed',
+}
+
+export enum TaskPriority {
+  Low = 'low',
+  Medium = 'medium',
+  High = 'high',
+}
+
+export interface Task {
+  id: number;
+  title: string;
+  description?: string;
+  status: TaskStatus;
+  priority?: TaskPriority;
+  assignees: Assignee[];
+  dueDate: string;
+  tags?: Tag[];
+}
+
+export enum Tag {
+  Design = 'design',
+  Dev = 'dev',
+  Marketing = 'marketing',
+  Bug = 'bug',
+  Business = 'business',
+  Legal = 'legal',
+}
